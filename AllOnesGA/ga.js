ae546ff6ef21be3dcd102e5b60b473219b1a50b4
sheshum <36011215+sheshum@users.noreply.github.com>
2018-09-28 18:24:41 +0200

    first working version

--- AllOnesGA/ga.js
+++ AllOnesGA/ga.js
@@ -1,8 +1,53 @@
+/**
+ * The GeneticAlgorithm class is our main abstraction for managing the
+ * operations of the genetic algorithm. This class is meant to be
+ * problem-specific, meaning that (for instance) the "calcFitness" method may
+ * need to change from problem to problem.
+ * 
+ * This class concerns itself mostly with population-level operations, but also
+ * problem-specific operations such as calculating fitness, testing for
+ * termination criteria, and managing mutation and crossover operations (which
+ * generally need to be problem-specific as well).
+ * 
+ * Generally, GeneticAlgorithm might be better suited as an abstract class or an
+ * interface, rather than a concrete class as below. A GeneticAlgorithm
+ * interface would require implementation of methods such as
+ * "isTerminationConditionMet", "calcFitness", "mutatePopulation", etc, and a
+ * concrete class would be defined to solve a particular problem domain. For
+ * instance, the concrete class "TravelingSalesmanGeneticAlgorithm" would
+ * implement the "GeneticAlgorithm" interface. This is not the approach we've
+ * chosen, however, so that we can keep each chapter's examples as simple and
+ * concrete as possible.
+ * 
+ */
+
+
 "use strict";
 
 var Population = require('./population.js');
 var Individual = require('./individual.js');
 
+/**
+ * Mutation rate is the fractional probability than an individual gene will
+ * mutate randomly in a given generation. The range is 0.0-1.0, but is
+ * generally small (on the order of 0.1 or less).
+ * @param mutationRate
+ */
+
+/**
+ * Crossover rate is the fractional probability that two individuals will
+ * "mate" with each other, sharing genetic information, and creating
+ * offspring with traits of each of the parents. Like mutation rate the
+ * rance is 0.0-1.0 but small.
+ * @param crossoverRate
+ */
+
+/**
+ * Elitism is the concept that the strongest members of the population
+ * should be preserved from generation to generation. If an individual is
+ * one of the elite, it will not be mutated or crossover.
+ * @param elitismCount
+ */
 var GeneticAlgorithm = function(populationSize, mutationRate, crossoverRate, elitismCount) {
     this.populationSize = populationSize;
     this.mutationRate = mutationRate;
@@ -33,11 +78,12 @@ var GeneticAlgorithm = function(populationSize, mutationRate, crossoverRate, eli
             throw Error("Expected argument: object of type [\'Population\']");
         } else {
             var _individuals = population.getIndividuals();
-            _individuals.forEach(member => {
-                if(member.getFitness() == 1) {
+            for(let geneIndex = 0; geneIndex < _individuals.length;
+            geneIndex++) {
+                if(_individuals[geneIndex].getFitness() == 1) {
                     return true;
                 }
-            });
+            }
 
             return false;
         }
@@ -59,7 +105,7 @@ var GeneticAlgorithm = function(populationSize, mutationRate, crossoverRate, eli
             if(this.crossoverRate > Math.random() && populationIndex > this.elitismCount) {
                 // Initialize offspring
                 var offspring = new Individual();
-                offspring.init(parent1.getChromosomeLength());
+                //offspring.init(parent1.getChromosomeLength());
 
                 // Find second parent
                 var parent2 = selectParent(population);
@@ -68,7 +114,7 @@ var GeneticAlgorithm = function(populationSize, mutationRate, crossoverRate, eli
                 for(let geneIndex = 0; geneIndex < parent1.getChromosomeLength(); geneIndex++) {
                     // Use half og parent1's genes and half of 
                     // parent2's genes
-                    if(Math.random() < 0.5) {
+                    if(0.5 > Math.random()) {
                         offspring.setGene(geneIndex, parent1.getGene(geneIndex));
                     } else {
                         offspring.setGene(geneIndex, parent2.getGene(geneIndex));
@@ -87,8 +133,43 @@ var GeneticAlgorithm = function(populationSize, mutationRate, crossoverRate, eli
         return _newPopulation;
     }
 
+    this.mutatePopulation = function(population) {
+        var _newPopulation = new Population();
 
-}
+        // Loop over current population by fitness
+        for(let populationIndex = 0; populationIndex < population.getSize(); 
+        populationIndex++) {
+            var _individual = population.getFittest(populationIndex);
+
+            // Loop over individual's genes
+            for(let geneIndex = 0; geneIndex < _individual.getChromosomeLength(); 
+            geneIndex++) {
+                // Skip mutation if this is an elite individual
+                if(populationIndex >= elitismCount) {
+                    // Does this gene need mutation?
+                    if (this.mutationRate > Math.random()) {
+                        // Get new gene
+                        var _newGene = 1;
+                        if(_individual.getGene(geneIndex) == 1) {
+                            _newGene = 0;
+                        }
+
+                        // Mutate gene
+                        _individual.setGene(geneIndex, _newGene);
+                    }
+                }
+            }
+
+            // Add individual to population
+            _newPopulation.setIndividual(populationIndex, _individual);
+        }
+
+        // Return mutated population
+        return _newPopulation;
+    }
+
+
+} // CLASS END
 
 function calcFitness(individual) {
     var correctGenes = 0;
@@ -114,12 +195,12 @@ function selectParent(population) {
 
     // Find parent
     var spinWheel = 0;
-    _individuals.forEach(individual => {
-        spinWheel += individual.getFitness();
+    for (let i = 0; i < _individuals.length; i++) {
+        spinWheel += _individuals[i].getFitness();
         if(spinWheel >= rouletteWheelPosition) {
-            return individual;
+            return _individuals[i];
         }
-    });
+    }
 
     return _individuals[population.getSize() - 1];
 }
--- AllOnesGA/main.js
+++ AllOnesGA/main.js
@@ -1,16 +1,45 @@
+/**
+ * This is our main module used to run the genetic algorithm.
+ * 
+ * This case is one of the simplest problems we can solve: the objective is to
+ * end up with an individual whose chromosome is all ones.
+ * 
+ * The simplicity of this problem makes the GeneticAlgorithm class'
+ * "calcFitness" method very simple. We'll just count the number of ones in the
+ * chromosome and use that as the fitness score. Similarly, the
+ * "isTerminationConditionMet" method in the GeneticAlgorithm class for this
+ * example is very simple: if the fitness score (ie, number of ones) is the same
+ * as the length of the chromosome (ie, we're all ones), we're done!
+ *
+ */
+
 "use strict";
 
 var GeneticAlgorithm = require('./ga.js');
 
-var ga = new GeneticAlgorithm(5, 0.01, 0.95, 0);
-var population = ga.initPopulation(10);
+// Create GA object
+var ga = new GeneticAlgorithm(100, 0.001, 0.95, 2);
 
+// Initialize population
+var population = ga.initPopulation(50);
+
+// Evaluate population
 ga.evalPopulation(population);
 
 // Keep track of current generation
 var generation  = 1;
 
-while ( ga.isTerminationConditionMet(population) == false && generation < 5 ) {
+/**
+ * Start the evolution loop
+ * 
+ * Every genetic algorithm problem has different criteria for finishing.
+ * In this case, we know what a perfect solution looks like (we don't
+ * always!), so our isTerminationConditionMet method is very
+ * straightforward: if there's a member of the population whose
+ * chromosome is all ones, we're done!
+ */
+while ( ga.isTerminationConditionMet(population) == false ) {
+    // Print fittest individual from population
     console.log(`Generation: ${generation}, Best solution: ${JSON.stringify(population.getFittest(0))}`);
 
     // Apply crossover
@@ -18,7 +47,7 @@ while ( ga.isTerminationConditionMet(population) == false && generation < 5 ) {
 
 
     // Apply mutation
-    // TODO!
+    population = ga.mutatePopulation(population);
 
 
     // Evaluate population
@@ -28,5 +57,10 @@ while ( ga.isTerminationConditionMet(population) == false && generation < 5 ) {
     generation++;   
 }
 
+/**
+ * We're out of the loop now, which means we have a perfect solution on
+ * our hands. Let's print it out to confirm that it is actually all
+ * ones, as promised.
+ */
 console.log(`Found solution in ${generation} generations.`);
 console.log(`Best solution: ${JSON.stringify(population.getFittest(0))}`);
