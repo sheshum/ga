610426e4f62282b13e50a9cc9b0cc37829cdc9d2
sheshum <36011215+sheshum@users.noreply.github.com>
2018-09-29 11:58:52 +0200

    added comments

--- AllOnesGA/ga.js
+++ AllOnesGA/ga.js
@@ -60,6 +60,17 @@ var GeneticAlgorithm = function(populationSize, mutationRate, crossoverRate, eli
         return population; 
     }
 
+    /**
+	 * Evaluate the whole population
+	 * 
+	 * Essentially, loop over the individuals in the population, calculate the
+	 * fitness for each, and then calculate the entire population's fitness. The
+	 * population's fitness may or may not be important, but what is important
+	 * here is making sure that each individual gets evaluated.
+	 * 
+	 * @param population
+	 *            the population to evaluate
+	 */
     this.evalPopulation = function(population) {
         if(population.__proto__.constructor.name !== 'Population') {
             throw Error("Expected argument: object of type [\'Population\']");
@@ -73,6 +84,15 @@ var GeneticAlgorithm = function(populationSize, mutationRate, crossoverRate, eli
         }   
     }
 
+    /**
+	 * Check if population has met termination condition
+	 * 
+	 * For this simple problem, we know what a perfect solution looks like, so
+	 * we can simply stop evolving once we've reached a fitness of one.
+	 * 
+	 * @param population
+	 * @return boolean True if termination condition met, otherwise, false
+	 */
     this.isTerminationConditionMet = function(population) {
         if(population.__proto__.constructor.name !== 'Population') {
             throw Error("Expected argument: object of type [\'Population\']");
@@ -89,8 +109,28 @@ var GeneticAlgorithm = function(populationSize, mutationRate, crossoverRate, eli
         }
     }
 
-    
-
+    /**
+	 * Apply crossover to population
+	 * 
+	 * Crossover, more colloquially considered "mating", takes the population
+	 * and blends individuals to create new offspring. It is hoped that when two
+	 * individuals crossover that their offspring will have the strongest
+	 * qualities of each of the parents. Of course, it's possible that an
+	 * offspring will end up with the weakest qualities of each parent.
+	 * 
+	 * This method considers both the GeneticAlgorithm instance's crossoverRate
+	 * and the elitismCount.
+	 * 
+	 * The type of crossover we perform depends on the problem domain. We don't
+	 * want to create invalid solutions with crossover, so this method will need
+	 * to be changed for different types of problems.
+	 * 
+	 * This particular crossover method selects random genes from each parent.
+	 * 
+	 * @param population
+	 *            The population to apply crossover to
+	 * @return The new population
+	 */
     this.crossoverPopulation = function(population) {
 
         // Create new population
@@ -133,6 +173,22 @@ var GeneticAlgorithm = function(populationSize, mutationRate, crossoverRate, eli
         return _newPopulation;
     }
 
+    /**
+	 * Apply mutation to population
+	 * 
+	 * Mutation affects individuals rather than the population. We look at each
+	 * individual in the population, and if they're lucky enough (or unlucky, as
+	 * it were), apply some randomness to their chromosome. Like crossover, the
+	 * type of mutation applied depends on the specific problem we're solving.
+	 * In this case, we simply randomly flip 0s to 1s and vice versa.
+	 * 
+	 * This method will consider the GeneticAlgorithm instance's mutationRate
+	 * and elitismCount
+	 * 
+	 * @param population
+	 *            The population to apply mutation to
+	 * @return The mutated population
+	 */
     this.mutatePopulation = function(population) {
         var _newPopulation = new Population();
 
@@ -169,8 +225,24 @@ var GeneticAlgorithm = function(populationSize, mutationRate, crossoverRate, eli
     }
 
 
-} // CLASS END
+}
 
+
+
+/**
+ * Calculate fitness for an individual.
+ * 
+ * In this case, the fitness score is very simple: it's the number of ones
+ * in the chromosome. Don't forget that this method, and this whole
+ * GeneticAlgorithm class, is meant to solve the problem in the "AllOnesGA"
+ * class and example. For different problems, you'll need to create a
+ * different version of this method to appropriately calculate the fitness
+ * of an individual.
+ * 
+ * @param individual
+ *            the individual to evaluate
+ * @return double The fitness value for individual
+ */
 function calcFitness(individual) {
     var correctGenes = 0;
     for(let geneIndex = 0; geneIndex < individual.getChromosomeLength(); 
@@ -184,6 +256,13 @@ function calcFitness(individual) {
     return fitness;
 }
 
+/**
+ * Select parent for crossover
+ * 
+ * @param population
+ *            The population to select parent from
+ * @return The individual selected as a parent
+ */
 function selectParent(population) {
 
     // Get individuals
