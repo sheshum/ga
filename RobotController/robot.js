403354351ef9a729bd00614ea75bf07ec5c5776e
sheshum <36011215+sheshum@users.noreply.github.com>
2018-10-05 22:09:03 +0200

    robot.js new changes

--- RobotController/robot.js
+++ RobotController/robot.js
@@ -16,17 +16,105 @@ class Robot {
      * @param maxMoves The maximum number of moves the robot can take
      */
     constructor(sensorActions, maze, maxMoves) {
+        this.DIRECTION = { N: 'north', S: 'south', E: 'east', W: 'west'};
+        this.sensorActions = this.calcSensorActions(sensorActions);
+        this.maze = maze;
+        let startPos = this.maze.getStartPosition();
+        this.xPosition = startPosition[0];
+        this.yPosition = startPosition[1];
 
+        this.sensorVal = -1;
+        this.heading = this.DIRECTION.N;
+        this.maxMoves = maxMoves;
+        this.moves = 0;
+        this.route = [];
+        this.route.push(startPos);
     }
 
-    run() {}
+    run() {
+        while(true) {
+            this.moves++;
+
+            return;
+        }
+    }
     calcSensorActions(sensorActionsStr) {}
     makeNextAction() {}
     getNextAction() {}
-    getSensorValue() {}
-    getPosition() {}
-    getHeading() {}
-    getRoute() {}
+
+
+    /**
+     * Get sensor value
+     * 
+     * @returns int Next sensor value
+     */
+    getSensorValue() {
+        // If sensor value has already been calculated
+        if(this.sensorVal > -1) {
+            return this.sensorVal;
+        }
+
+        let frontSensor, frontLeftSensor, frontRightSensor,
+            leftSensor, rightSensor, backSensor;
+
+        frontSensor = frontLeftSensor = frontRightSensor = leftSensor =
+            rightSensor = backSensor = false;
+
+        // Find which sensor have been activated
+        if(this.getHeading() == this.DIRECTION.N) {
+
+        }
+        else if(this.getHeading() == this.DIRECTION.E) {
+            
+        }
+        else if(this.getHeading() == this.DIRECTION.S) {
+            
+        }
+        else if(this.getHeading() == this.DIRECTION.W) {
+            
+        }
+        else {
+
+        }
+
+        // Calculate sensor value
+        let sensorVal = 0;
+
+        if (frontSensor == true) {
+            sensorVal += 1;
+        }
+        if (frontLeftSensor == true) {
+            sensorVal += 2;
+        }
+        if (frontRightSensor == true) {
+            sensorVal += 4;
+        }
+        if (leftSensor == true) {
+            sensorVal += 8;
+        }
+        if (rightSensor == true) {
+            sensorVal += 16;
+        }
+        if (backSensor == true) {
+            sensorVal += 32;
+        }
+
+        this.sensorVal = sensorVal;
+    }
+
+
+    getPosition() {
+        return [this.xPosition, this.yPosition];
+    }
+
+    getHeading() {
+        return this.heading;
+    }
+
+    getRoute() {
+        return this.route;
+    }
+
     printRoute() {}
 }
 
